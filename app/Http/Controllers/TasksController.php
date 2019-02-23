1ccb77896e47052d25834c204802f5aa35985f85
takeshan <takeshan2001@yahoo.co.jp>
2019-02-23 09:00:27 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,7 +43,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   
+            'content' => 'required|max:191',
+        ]);
+        
+        
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -89,10 +97,18 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   
+            'content' => 'required|max:191',
+        ]);
+        
+        
+        
         $task = task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
-
+       
         return redirect('/');
     }
 
