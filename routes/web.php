cb375a6907439c9340afca1edd738f003da89893
takeshan <takeshan2001@yahoo.co.jp>
2019-03-10 15:02:35 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,20 @@
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('/', 'TasksController@index');
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
 Route::resource('tasks', 'TasksController');
+
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
